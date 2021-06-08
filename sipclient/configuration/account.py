d87a73149bd506dc61b8620ac05bef99c0165b1a
Adrian Georgescu <ag@ag-projects.com>
2021-06-08 13:39:27 +0000

    Added CPIM and IMDN payloads to sip-message script

--- sipclient/configuration/account.py
+++ sipclient/configuration/account.py
@@ -17,8 +17,15 @@ class SoundsSettings(SettingsGroup):
     audio_inbound = Setting(type=AccountSoundFile, default=AccountSoundFile(AccountSoundFile.DefaultSoundFile('sounds.audio_inbound')), nillable=True)
 
 
+class SMSSettings(SettingsGroup):
+    use_cpim = Setting(type=bool, default=True)
+    enable_imdn = Setting(type=bool, default=True)
+
+
 class AccountExtension(SettingsObjectExtension):
     rtp = RTPSettingsExtension
     sounds = SoundsSettings
+    sms = SMSSettings
 
 
+    
